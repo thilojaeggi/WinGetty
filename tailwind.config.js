3778aeb2034bfae1c375275a59e6d9c0a6e53f09
Thilo Jaeggi <thilo0123@gmail.com>
2023-06-12 07:25:11 +0000

    Change tailwind scope to include subdirs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.{html,js,j2}"],
+  content: ["./templates/**/*.{html,js,j2}"],
   theme: {
     extend: {},
   },
