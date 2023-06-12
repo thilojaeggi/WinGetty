3fde7fcf982f7bb527ca3631d31a606a23c50f52
Thilo Jaeggi <thilo0123@gmail.com>
2023-06-12 11:30:29 +0000

    Add file saving to own function

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import hashlib
+import os
 from flask import current_app
+from app import basedir
 
 def calculate_sha256(filename):
     sha256_hash = hashlib.sha256()
@@ -14,3 +16,16 @@ def calculate_sha256(filename):
 def debugPrint(message):
     if current_app.config['DEBUG']:
         print(message)
+
+def save_file(file, publisher, identifier, version, architecture):
+    # Create directory if it doesn't exist
+    save_directory = os.path.join(basedir, 'packages', publisher, identifier, version, architecture)
+        # Create directory if it doesn't exist
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+    # Save file
+    file_path = os.path.join(save_directory, file.filename)
+    file.save(file_path)
+    # Get file hash
+    hash = calculate_sha256(file_path)
+    return hash
