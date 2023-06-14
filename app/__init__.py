047543694fb4e4df1bc3373996423825b62c36f7
Thilo Jaeggi <thilo0123@gmail.com>
2023-06-14 13:37:50 +0000

    Add flask-htmx library

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, current_app
 from flask_sqlalchemy import SQLAlchemy
+from flask_htmx import HTMX
 from datetime import datetime
 from distutils.version import LooseVersion
 from . import config
 from .utils import basedir
 db = SQLAlchemy()
+htmx = HTMX()
 
 def sort_versions(versions):
     return sorted(versions, key=lambda x: LooseVersion(x.version_code), reverse=True)
@@ -15,6 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config.Config)
     db.init_app(app)
+    htmx.init_app(app)
 
     from app.ui_routes import ui
     from app.api_routes import api
