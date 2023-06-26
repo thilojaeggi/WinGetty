0a0b1b2c022d206b11b343c596b76f65973c5dd2
Thilo Jaeggi <thilo0123@gmail.com>
2023-06-26 23:42:27 +0200

    Pass env

--- app/ui_routes.py
+++ app/ui_routes.py
@@ -2,7 +2,7 @@ from app import db, htmx
 from flask import Blueprint, jsonify, render_template, request, redirect, url_for
 from app.models import Package, PackageVersion, Installer
 from app.utils import debugPrint
-
+import os
 ui = Blueprint('ui', __name__)
 
 @ui.route('/')
@@ -59,4 +59,5 @@ def settings():
 @ui.route('/package/<identifier>', methods=['GET'])
 def package(identifier):
     package = Package.query.filter_by(identifier=identifier).first()
-    return render_template('package.j2', package=package)
+
+    return render_template('package.j2', package=package,env=os.environ)
