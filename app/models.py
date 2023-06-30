6c8ac873fa1d6f0748e43287e8a14d598f9ed2bd
Thilo Jaeggi <thilo0123@gmail.com>
2023-06-30 16:57:50 +0200

    Add set_password func

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, bcrypt
 from flask import url_for, current_app
 import os
 from flask_login import UserMixin
@@ -108,6 +108,12 @@ class InstallerSwitch(db.Model):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+
+    def set_password(self, password):
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password = hashed_password
+        
+
