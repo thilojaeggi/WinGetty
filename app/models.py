40ea3c97b462d103cf1650f85b67e48679aadc93
Thilo Jaeggi <thilo0123@gmail.com>
2023-07-01 15:08:11 +0200

    Rename name to username

--- app/models.py
+++ app/models.py
@@ -108,7 +108,7 @@ class InstallerSwitch(db.Model):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    username = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
