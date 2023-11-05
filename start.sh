288ee5f6f413917626cd9cf699531724e74d9ff9
Thilo <thilo0123@gmail.com>
2023-11-05 05:14:26 +0100

    Add log level to gunicorn

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 flask db upgrade
-exec gunicorn -b :8080 --workers 4 --worker-class gevent "app:create_app()"
+# Get log level from env variable
+if [ -z "$LOG_LEVEL" ]; then
+    LOG_LEVEL=info
+fi
+# Start Gunicorn processes
+echo Starting Gunicorn with $LOG_LEVEL log level
+exec gunicorn -b :8080 --workers 4 --worker-class gevent "app:create_app()" --log-level=$LOG_LEVEL
