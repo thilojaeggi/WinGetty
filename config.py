e224dda17c758a42c75be5d4d866882e676da9e7
Thilo <thilo0123@gmail.com>
2023-11-12 21:40:02 +0100

    Bump version number

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="WINGETTY",
     settings_files=['settings.toml', '.secrets.toml'],
-    VERSION = "0.0.7",
+    VERSION = "0.0.8",
     PREFERRED_URL_SCHEME = "https"
 )
 
