0b7a4651aba6a92a46e77040f5a49090519e9e75
Thilo <thilo0123@gmail.com>
2024-03-18 12:20:44 +0100

    Remove unneeded code since api gets data

--- app/ui_routes.py
+++ app/ui_routes.py
@@ -15,40 +15,8 @@ def index():
 @login_required
 @permission_required('view:package')
 def packages():
-    page = request.args.get('page', 1, type=int)
-    search_query = request.args.get('q')
-    per_page = 10
 
-    if search_query:
-        packages = Package.query.filter(
-            db.or_(
-                Package.name.ilike(f'%{search_query}%'),
-                Package.identifier.ilike(f'%{search_query}%')
-            )
-        )
-    else:
-        packages = Package.query
-
-    total_pages = packages.paginate(page=1, per_page=per_page).pages
-    if page < 1 or (total_pages > 0 and page > total_pages):
-        return redirect(url_for('ui.packages', q=search_query, page=total_pages))
-
-    packages_paginated = packages.paginate(page=page, per_page=per_page)
-    available_pages = packages_paginated.pages
-    packages = packages_paginated.items
-
-    # Get total item count available
-    package_count = packages_paginated.total
-
-    current_app.logger.debug(f'Page: {page}')
-    current_app.logger.debug(f'Available pages: {available_pages}')
-
-    if htmx:
-        return render_template('packages_rows.j2', packages=packages)
-    
-    
-
-    return render_template('packages.j2', packages=packages, page=page, pages=available_pages, package_count=package_count)
+    return render_template('packages.j2')
 
 
 @ui.route('/setup')
@@ -58,7 +26,7 @@ def setup():
 
 @ui.route('/settings')
 @login_required
-#@permission_required('view:settings')
+@permission_required('view:settings')
 def settings():
     settings = db.session.query(Setting).all()
     return render_template('settings.j2', settings=settings)
@@ -78,7 +46,5 @@ def users():
 @login_required
 @permission_required('view:package')
 def package(identifier):
-    package = Package.query.filter_by(identifier=identifier).first()
-    if not package:
-        return render_template('error/404.j2'), 404
+
     return render_template('package.j2', package=package)
