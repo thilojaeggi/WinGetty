2300efe29cb7056277e4fe178949cbb9e8f7e082
Thilo <thilo0123@gmail.com>
2024-04-24 12:39:36 +0200

    add oidc support

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,26 @@ import json
 import logging
 import os
 import sys
+import semver
+
+from flask_caching import Cache
+import requests
 from . import constants
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, current_app
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from flask_sqlalchemy import SQLAlchemy
 from flask_htmx import HTMX
 from datetime import datetime
-from distutils.version import LooseVersion
+from packaging.version import parse as parse_version
+from authlib.integrations.flask_client import OAuth
 
 from sqlalchemy import MetaData
-from config import settings
+from config import Config
 from dynaconf import FlaskDynaconf
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-ascii_logo = """
+ascii_logo = r"""
  _       ___       ______     __  __       
 | |     / (_)___  / ____/__  / /_/ /___  __
 | | /| / / / __ \/ / __/ _ \/ __/ __/ / / /
@@ -34,16 +39,33 @@ convention = {
 }
 metadata = MetaData(naming_convention=convention)
 
+oauth = None
+
+
+
 db = SQLAlchemy(metadata=metadata)
 htmx = HTMX()
 dynaconf = FlaskDynaconf()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 migrate = Migrate()
+cache = Cache()
+oauth = OAuth()
+
+def init_oidc(app):
+    oauth = OAuth(app)
+    app.oidc_provider = oauth.register(
+        name='oidc',
+        client_id=app.config['OIDC_CLIENT_ID'],
+        client_secret=app.config['OIDC_CLIENT_SECRET'],
+        server_metadata_url=app.config['OIDC_SERVER_METADATA_URL'],
+        client_kwargs={'scope': 'openid email profile'},
+    )
+    app.extensions['oauth'] = oauth
 
 
 def sort_versions(versions):
-    return sorted(versions, key=lambda x: LooseVersion(x.version_code), reverse=True)
+    return sorted(versions, key=lambda x: parse_version(x), reverse=True)
 
 def page_not_found(e):
   return render_template('error/404.j2',error=True), 404
@@ -54,8 +76,6 @@ def internal_server_error(e):
 def favicon():
     return url_for('static', filename='img/favicon.ico')
 
-def current_year():
-    return {'now': datetime.now}
 
 def remove_none_values(value):
     if isinstance(value, list):
@@ -66,6 +86,9 @@ def remove_none_values(value):
         return value
 
 
+
+
+
 class PrefixLoggerAdapter(logging.LoggerAdapter):
     """ A logger adapter that adds a prefix to every message """
     def process(self, msg: str, kwargs: dict) -> (str, dict):
@@ -73,8 +96,13 @@ class PrefixLoggerAdapter(logging.LoggerAdapter):
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    Config.init_app(app)
+
+    if os.getenv('WINGETTY_SQLALCHEMY_DATABASE_URI'):
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('WINGETTY_SQLALCHEMY_DATABASE_URI')
+
     
-    app.config.from_object(settings)
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(500, internal_server_error)
 
@@ -82,8 +110,50 @@ def create_app():
     from app.models import User, Package, PackageVersion, Installer, InstallerSwitch, Permission, Role, Setting
     migrate.init_app(app, db)
     htmx.init_app(app)
-    dynaconf.init_app(app)
     bcrypt.init_app(app)
+    app.config['CACHE_TYPE'] = 'SimpleCache'
+    cache.init_app(app)
+    if app.config.get('OIDC_ENABLED'):
+        init_oidc(app)
+
+    def get_latest_version():
+        # Try to get cached version information first
+        version_info = cache.get('version_info')
+        if version_info is not None:
+            return version_info
+
+        # If not in cache, fetch from GitHub API
+        try:
+            response = requests.get("https://api.github.com/repos/thilojaeggi/WinGetty/releases/latest")
+            response.raise_for_status()
+            data = response.json()
+            latest_version = data["tag_name"].lstrip("v")
+            current_version = current_app.config["VERSION"]
+
+            latest_version = semver.Version.parse(latest_version)
+            current_version = semver.Version.parse(current_version)
+
+            # Check if the latest version is newer than the current version using semver
+            if semver.Version.compare(latest_version, current_version) > 0:
+                version_info = {
+                    "current_version": current_version,
+                    "latest_version": latest_version,
+                    "update_available": True
+                }
+            else:
+                version_info = {
+                    "update_available": False
+                }
+
+            # Cache the result for 24 hours as to not spam the GitHub API with requests
+            cache.set('version_info', version_info, timeout=86400)
+            return version_info
+        except requests.RequestException as e:
+            # In case of an error, provide an error message but report no update available
+            return {
+                "update_available": False,
+                "error": str(e)
+            }
 
     login_manager.login_view = 'auth.login'
     login_manager.login_message = 'You must be logged in to access this page.'
@@ -96,7 +166,7 @@ def create_app():
 
     from app.ui_routes import ui
     from app.api_routes import api
-    from app.auth_routes import auth
+    from app.auth import auth
     from app.winget_routes import winget
 
     app.register_blueprint(ui)
@@ -137,24 +207,28 @@ def create_app():
     @app.context_processor
     def inject_settings():
         return dict(global_settings=get_settings())
+    
+    @app.context_processor
+    def inject_version():
+        return dict(version_info=get_latest_version())
 
     @app.context_processor
     def inject_now():
-        return {'now': datetime.utcnow()}
+        return {'now': datetime.now()}
     
     
     @app.route('/favicon.ico')
     def favicon():
         return url_for('static', filename='img/favicon.ico')
     
-    # Hacky way to not trigger permissions creation on flask db upgrade as db is not yet initialized
-    if not 'flask' in sys.argv and not 'db' in sys.argv:
+
+    
+    # Hacky way to not trigger permissions/settings creation on flask db upgrade or testing as db is not yet initialized
+    if 'run' in sys.argv:
         with app.app_context():
             from app.permissions import create_all
             create_all()
             from app.settings import create_all
             create_all()
-
         
-
     return app
